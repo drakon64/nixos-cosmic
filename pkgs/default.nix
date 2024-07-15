4ab99e2774345753732611c54ab8f5385928f817
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-07-15 11:01:44 +0100

    Add missing `pkgs/default.nix` changes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,37 @@
-{ pkgs ? null, final ? null, prev ? null }:
+{
+  pkgs,
+  customRustPlatform,
+}:
 
 let
   dir = builtins.readDir ./.;
-  pkgsPaths = builtins.filter (pkgPath: pkgPath != null) (builtins.map (name: if dir.${name} == "directory" then { inherit name; value = "${builtins.toString ./.}/${name}/package.nix"; } else null) (builtins.attrNames dir));
-  callPackage = if final != null then final.callPackage else pkgs.lib.callPackageWith (pkgs // finalPkgs // { buildPackages = finalPkgs; targetPackages = finalPkgs; inherit callPackage; });
-  finalPkgs = builtins.listToAttrs (builtins.map (pkgPath: { inherit (pkgPath) name; value = callPackage pkgPath.value {}; }) pkgsPaths);
-in finalPkgs
+  pkgsPaths = builtins.filter (pkgPath: pkgPath != null) (
+    builtins.map (
+      name:
+      if dir.${name} == "directory" then
+        {
+          inherit name;
+          value = "${builtins.toString ./.}/${name}/package.nix";
+        }
+      else
+        null
+    ) (builtins.attrNames dir)
+  );
+  callPackage = pkgs.lib.callPackageWith (
+    pkgs
+    // finalPkgs
+    // {
+      buildPackages = finalPkgs;
+      targetPackages = finalPkgs;
+      inherit callPackage;
+      rustPlatform = customRustPlatform;
+    }
+  );
+  finalPkgs = builtins.listToAttrs (
+    builtins.map (pkgPath: {
+      inherit (pkgPath) name;
+      value = callPackage pkgPath.value {};
+    }) pkgsPaths
+  );
+in
+finalPkgs
